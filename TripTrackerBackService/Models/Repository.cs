1076c7af981067390c2072422ff9ec01d20502c2
Dmnoz <Dimentor77>
2019-07-19 20:59:12 +0800

    Add project files.

---
+++ TripTrackerBackService/Models/Repository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTrackerBackService.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                id = 1,
+                Name = "MVP Summit",
+                StartDate  = new DateTime(2018, 3, 5),
+                EndDate = new DateTime(2018, 3, 8)
+            },
+              new Trip
+            {
+                id = 2,
+                Name = "DevIntersection Orlando 2018",
+                StartDate  = new DateTime(2018, 3, 25),
+                EndDate = new DateTime(2018, 3, 27)
+            },
+                 new Trip
+            {
+                id = 3,
+                Name = "Build 2018",
+                StartDate  = new DateTime(2018, 5, 7),
+                EndDate = new DateTime(2018, 5, 9)
+            }
+        };
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+        public Trip Get(int id)
+        {
+            return MyTrips.First(t => t.id == id);
+        }
+
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.id == tripToUpdate.id));
+            Add(tripToUpdate);
+        }
+
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.id == id));
+
+        }
+    }
+}
---
+++ TripTrackerBackService/Models/Segment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTrackerBackService.Models
+{
+    public class Segment
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Tripid { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset StartDateTime { get; set; }
+
+
+        public DateTimeOffset EndDateTime { get; set; }
+
+        //public void Test()
+        //{
+        //    StartDateTime.Offset
+        //}
+    }
+}
